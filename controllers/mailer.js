bf29fc55ed775d098aa438c6350a4ea9655c5414
Martin Chammah <martin.chammah@ecomerciar.com>
2020-12-03 09:51:14 -0300

    mailer info

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -15,17 +15,38 @@ const mail = async (terms, email) => {
     });
     
     let termy = JSON.stringify(terms);
+    
 
-    const html = `
-    <div>
-    <h4>Found new differences for</h4>
-    ${termy}
-    <br>
-    check them out @ <a href="localhost:5001/difference"> HERE </a>
+    // const html = `
+    // <div>
+    // <h4>Found new differences for</h4>
+    // ${termy}
+    // <br>
+    // check them out @ <a href="localhost:5001/difference"> HERE </a>
     
-    </div>
+    // </div>
     
-    `
+    // `
+
+    let html = '<div>' + terms.map((item)=> {
+        let intItems = item.data.map((subitem) => {
+            return(
+                `
+                <div>
+                    <h3>${subitem.title} <strong style="color: red;">$${subitem.price}</strong></h3>
+                    <p>${subitem.city} ${subitem.state}</p>
+                    <a href="${subitem.link}">Link</a>
+                </div>
+                `
+            )
+        })
+        return(
+            `<div style="max-width: 200px; background-color: #ccc; border: 1px solid black; margin: 20px 10px">
+                ${intItems}
+            </div>`
+        )
+    }).join('') + '</div>' + "\n";
+
 
     // mensaje del nodemailer
     const message = await transporter.sendMail({
